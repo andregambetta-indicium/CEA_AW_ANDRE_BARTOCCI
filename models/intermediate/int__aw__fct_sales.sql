eccfb152e9f925322ded68b1249b0d71dd7b9d71
André Luiz Bartocci Gambetta <andre.gambetta@indicium.tech>
2026-04-19 23:41:40 +0000

    Corrigindo int_fct_sales e adicionando testes

--- models/intermediate/int__aw__fct_sales.sql
+++ models/intermediate/int__aw__fct_sales.sql
@@ -2,19 +2,29 @@ with sales_order_header as (
     select * from {{ ref('stg__aw__salesorderheader') }}
 ),
 
-sales_order_detail as (
-    select * from {{ ref('stg__aw__salesorderdetail') }}
+sales_order_detail_aggregated as (
+    select
+        salesorderid,
+        count(salesorderdetailid) as total_items,
+        sum(orderqty) as total_orderqty,
+        sum(unitprice * orderqty) as gross_total,
+        sum(unitprice * (1 - unitpricediscount) * orderqty) as net_total
+    from {{ ref('stg__aw__salesorderdetail') }}
+    group by 1
 ),
 
-sales_order_header_reason as (
-    select * from {{ ref('stg__aw__salesorderheadersalesreason') }}
+sales_reasons_aggregated as (
+    select
+        header_reason.salesorderid,
+        listagg(reason.name, ', ') as reasonname,
+        listagg(reason.reasontype, ', ') as reasontype
+    from {{ ref('stg__aw__salesorderheadersalesreason') }} header_reason
+    left join {{ ref('stg__aw__salesreason') }} reason 
+        on header_reason.salesreasonid = reason.salesreasonid
+    group by 1
 ),
 
-sales_reason as (
-    select * from {{ ref('stg__aw__salesreason') }}
-),
-
-int_sales as (
+int_fct_sales as (
     select
         header.salesorderid,
         header.customerid,
@@ -25,10 +35,6 @@ int_sales as (
         header.shipmethodid,
         header.creditcardid,
         header.currencyrateid,
-        detail.salesorderdetailid,
-        detail.productid,
-        detail.specialofferid,
-        header_reason.salesreasonid,
         header.orderdate,
         header.duedate,
         header.shipdate,
@@ -38,24 +44,22 @@ int_sales as (
         header.accountnumber,
         header.creditcardapprovalcode,
         header.revisionnumber,
-        detail.carriertrackingnumber,
-        reason.name as reasonname,
-        reason.reasontype,
-        detail.orderqty,
-        detail.unitprice,
-        detail.unitpricediscount,
+        reasons.reasonname,
+        reasons.reasontype,
+        detail.total_items,
+        detail.total_orderqty,
+        detail.gross_total,
+        detail.net_total,
         header.subtotal,
         header.taxamt,
         header.freight,
         header.totaldue
 
-    from sales_order_header as header
-    left join sales_order_detail as detail 
+    from sales_order_header header
+    left join sales_order_detail_aggregated detail 
         on header.salesorderid = detail.salesorderid
-    left join sales_order_header_reason as header_reason 
-        on header.salesorderid = header_reason.salesorderid
-    left join sales_reason as reason 
-        on header_reason.salesreasonid = reason.salesreasonid
+    left join sales_reasons_aggregated reasons 
+        on header.salesorderid = reasons.salesorderid
 )
 
-select * from int_sales
+select * from int_fct_sales
